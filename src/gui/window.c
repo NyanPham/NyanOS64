2886aeff29ba5f0ffc51468be669a875d827b1d0
phamthanhnhanussh@gmail.com <phamthanhnhanussh@gmail.com>
2025-12-25 22:56:23 +0700

    Implemented a Doubly Linked List to handle multiple windows at a time

--- src/gui/window.c
+++ src/gui/window.c
@@ -1,6 +1,7 @@
 #include "window.h"
 #include "drivers/video.h"
 #include "drivers/mouse.h"
+#include "mem/kmalloc.h"
 #include "kern_defs.h"
 #include "drivers/serial.h" // debugging
 
@@ -10,22 +11,14 @@
 #define WIN_H 150
 #define TITLE_H 20
 
+static Window *g_win_list = NULL; // Bottom / Head of lis
+static Window *g_win_top = NULL;  // Top / Tail of List to focus
+
 static WinDragCtx drag_ctx =
     {
         .target = NULL,
         .off_x = 0,
         .off_y = 0,
-};
-
-// test instance of window
-static Window win_test =
-    {
-        .x = 300,
-        .y = 200,
-        .width = 200,
-        .height = 150,
-        .title = "Test App",
-        .is_dragging = false,
 };
 
 static void draw_window(Window *win)
@@ -61,9 +54,39 @@ static void draw_window(Window *win)
     }
 }
 
+Window *create_win(int64_t x, int64_t y, uint64_t width, uint64_t height, const char *title)
+{
+    Window *win = (Window*)kmalloc(sizeof(Window));
+    win->x = x;
+    win->y = y;
+    win->width = width;
+    win->height = height;
+    win->title = title;
+    win->is_dragging = false;
+    win->next = NULL;
+
+    if (g_win_list == NULL || g_win_top == NULL)
+    {
+        win->prev = NULL;
+        g_win_list = win;
+        g_win_top = win;
+    }
+    else
+    {
+        win->prev = g_win_top;
+        g_win_top->next = win;
+        g_win_top = win;
+    }
+
+    return win;
+}
+
 void init_window_manager(void)
 {
-    draw_window(&win_test);
+    Window *win1 = create_win(300, 200, 200, 150, "Test App");
+    // Create second window to test
+    Window *win2 = create_win(350, 230, 250, 250, "Test App 2");
+
     kprint("Window manager inited!\n");
 }
 
@@ -94,7 +117,102 @@ void stop_window_drag(Window *win)
 
 void window_paint()
 {
-    draw_window(&win_test);
+    Window *curr = g_win_list;
+    while (curr != NULL)
+    {
+        draw_window(curr);
+        curr = curr->next;
+    }
+}
+
+Window *get_win_at(int64_t mx, int64_t my)
+{
+    Window *curr = g_win_top;
+
+    while (curr != NULL)
+    {
+        if (mx >= curr->x &&
+            mx < curr->x + curr->width &&
+            my >= curr->y &&
+            my < curr->y + curr->height)
+        {
+            return curr;
+        }
+        curr = curr->prev;
+    }
+
+    return NULL;
+}
+
+/**
+ * @brief Move the window to front
+ */
+void focus_win(Window *win)
+{
+    if (win == g_win_top)
+    {
+        return;
+    }
+
+    if (win == g_win_list)
+    {
+        g_win_top->next = g_win_list;
+        g_win_list->prev = g_win_top;
+        g_win_top = g_win_list;
+        g_win_list = g_win_list->next;
+        g_win_list->prev = NULL;
+        g_win_top->next = NULL;
+        return;
+    }
+
+    // unlink the win
+    if (win->prev != NULL && win->next != NULL)
+    {
+        win->prev->next = win->next;
+        win->next->prev = win->prev;
+        win->next = NULL;
+    }
+
+    // relink to the tail
+    win->prev = g_win_top;
+    g_win_top->next = win;
+    g_win_top = win;
+}
+
+void close_win(Window *win)
+{
+    if (win == drag_ctx.target)
+    {
+        drag_ctx.target = NULL;
+    }
+
+    if (win->prev == NULL && win->next == NULL) // the only one left
+    {
+        g_win_list = NULL;
+        g_win_top = NULL;
+        kfree(win);
+        return;
+    }
+
+    if (win == g_win_list)
+    {
+        g_win_list->next->prev = NULL;
+        g_win_list = g_win_list->next;
+        kfree(win);
+        return;
+    }
+
+    if (win == g_win_top)
+    {
+        g_win_top->prev->next = NULL;
+        g_win_top = g_win_top->prev;
+        kfree(win);
+        return;
+    }
+
+    win->prev->next = win->next;
+    win->next->prev = win->prev;
+    kfree(win);
 }
 
 void window_update(void)
@@ -111,11 +229,19 @@ void window_update(void)
             drag_ctx.target->x = mx - drag_ctx.off_x;
             drag_ctx.target->y = my - drag_ctx.off_y;
         }
-        else if (check_window_drag(&win_test, mx, my))
+        else
         {
-            drag_ctx.target = &win_test;
-            drag_ctx.off_x = mx - win_test.x;
-            drag_ctx.off_y = my - win_test.y;
+            Window *curr_win = get_win_at(mx, my);
+            if (curr_win != NULL)
+            {
+                focus_win(curr_win);
+                if (check_window_drag(curr_win, mx, my))
+                {
+                    drag_ctx.target = curr_win;
+                    drag_ctx.off_x = mx - curr_win->x;
+                    drag_ctx.off_y = my - curr_win->y;
+                }
+            }
         }
     }
     else
--- src/gui/window.h
+++ src/gui/window.h
@@ -4,14 +4,19 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef struct
+typedef struct Window
 {
-    int x;
-    int y;
-    int width;
-    int height;
+    int64_t x;
+    int64_t y;
+    uint64_t width;
+    uint64_t height;
     char *title;
     bool is_dragging;
+
+    // Doubly Linked List to handle bring to front
+    // and back more easily
+    struct Window *prev;
+    struct Window *next;
 } Window;
 
 typedef struct WinDragCtx
@@ -25,7 +30,11 @@ void init_window_manager(void);
 void update_window_drag(Window *win, int16_t dx, int16_t dy);
 bool check_window_drag(Window *win, int64_t mouse_x, int64_t mouse_y);
 void stop_window_drag(Window *win);
-void window_paint();
+void window_paint(void);
 void window_update(void);
+Window *create_win(int64_t x, int64_t y, uint64_t width, uint64_t height, const char *title);
+Window *get_win_at(int64_t mx, int64_t my);
+void focus_win(Window *win);
+void close_win(Window *win);
 
 #endif
